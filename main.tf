8b190b671e31c1c09d4a22dc9c9a79422d1e0738
Khang Tran <khangtna@trankhang.local>
2025-04-04 16:15:20 +0700

    Initial clean commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    vnpaycloud = {
+      source = "terraform-provider-vnpaycloud/vnpaycloud"
+      version = "1.0.0"
+    }
+  }
+}
+
+provider "vnpaycloud" {
+  auth_url = "aaa"
+  application_credential_id = "aaaa"
+  application_credential_name = "tf_dev_for_code"
+  application_credential_secret = "aaaaa"
+}
+
+resource "vnpaycloud_volume" "volume_created_by_tf_thuannt" {
+  name = "volume_created_by_tf_thuannt"
+  description = "volume_created_by_tf_thuannt"
+  size = 30
+  volume_type = "c1-standard"
+}
